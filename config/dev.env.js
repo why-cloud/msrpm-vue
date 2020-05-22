d13e5b30356bcfb9c058214625959d1709a590b0
mary <z535983297@icloud.com>
2020-05-22 18:13:41 +0900

    王婷第一次提交

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  BASE_API: '"http://localhost:8088"',
   OPEN_PROXY: false // 是否开启代理, 重置后需重启vue-cli
 })
